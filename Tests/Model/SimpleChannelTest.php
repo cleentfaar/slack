265d4f338e500a1147373851fecf1174aedcf9b8
Cas Leentfaar <git@casleentfaar.com>
2014-12-06 01:52:54 +0100

    test fixes

--- Tests/Model/SimpleChannelTest.php
+++ Tests/Model/SimpleChannelTest.php
@@ -25,10 +25,12 @@ class SimpleChannelTest extends AbstractModelTest
     protected function getModelData()
     {
         return [
-            'id'      => 'C1234567',
-            'name'    => 'channel_name',
-            'created' => '12345678',
-            'creator' => 'U1234567',
+            'id'          => 'C1234567',
+            'name'        => 'channel_name',
+            'created'     => '12345678',
+            'creator'     => 'U1234567',
+            'is_archived' => false,
+            'is_general'  => false,
         ];
     }
 
@@ -51,5 +53,7 @@ class SimpleChannelTest extends AbstractModelTest
         $this->assertEquals($expectedData['name'], $actualModel->getName());
         $this->assertEquals($expectedData['created'], $actualModel->getCreated()->format('U'));
         $this->assertEquals($expectedData['creator'], $actualModel->getCreator());
+        $this->assertEquals($expectedData['is_archived'], $actualModel->isArchived());
+        $this->assertEquals($expectedData['is_general'], $actualModel->isGeneral());
     }
 }
