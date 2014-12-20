9a3f7c2bcca6938da26e83fdff7a0172ea98e7e1
Cas Leentfaar <git@casleentfaar.com>
2014-12-21 00:34:08 +0100

    added more tests

---
+++ src/CL/Slack/Tests/Payload/ChannelsLeavePayloadResponseTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Slack API library.
+ *
+ * (c) Cas Leentfaar <info@casleentfaar.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CL\Slack\Tests\Payload;
+
+use CL\Slack\Payload\ChannelsLeavePayloadResponse;
+use CL\Slack\Payload\PayloadResponseInterface;
+
+/**
+ * @author Cas Leentfaar <info@casleentfaar.com>
+ */
+class ChannelsLeavePayloadResponseTest extends AbstractPayloadResponseTest
+{
+    /**
+     * @inheritdoc
+     */
+    protected function createResponseData()
+    {
+        return [
+            'not_in_channel' => true,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param array                        $responseData
+     * @param ChannelsLeavePayloadResponse $payloadResponse
+     */
+    protected function assertResponse(array $responseData, PayloadResponseInterface $payloadResponse)
+    {
+        $this->assertEquals($responseData['not_in_channel'], $payloadResponse->isNotInChannel());
+    }
+}
