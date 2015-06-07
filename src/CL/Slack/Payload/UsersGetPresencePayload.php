752fe94665451b589fdcc46b0c948d6f5b7f076e
Germain Carré <germaincarre@yahoo.fr>
2015-06-07 17:43:43 +0200

    adding users.getPresence

---
+++ src/CL/Slack/Payload/UsersGetPresencePayload.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Slack API library.
+ *
+ * (c) Cas Leentfaar <info@casleentfaar.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CL\Slack\Payload;
+
+/**
+ * @author Cas Leentfaar <info@casleentfaar.com>
+ *
+ * @link Official documentation at https://api.slack.com/methods/presence.set
+ */
+class UsersGetPresencePayload extends AbstractPayload
+{
+    /**
+     * @var string $user ID of the user
+     */
+    private $user;
+
+    /**
+     * @param string $user ID of the user
+     */
+    public function setUserId($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return string ID of the user
+     */
+    public function getUserId()
+    {
+        return $this->user;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMethod()
+    {
+        return 'users.getPresence';
+    }
+}
