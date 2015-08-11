bd806c39cbe235f7c4cd8184686e293474f3c4e4
Travis Raup <travis.raup@restek.com>
2015-08-11 13:54:17 -0400

    GroupsInfoPayload class missing. groups.info api method added to library.

---
+++ src/CL/Slack/Payload/GroupsInfoPayload.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Slack API library.
+ *
+ * (c) Cas Leentfaar <info@casleentfaar.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CL\Slack\Payload;
+
+/**
+ * @author Cas Leentfaar <info@casleentfaar.com>
+ *
+ * @link Official documentation at https://api.slack.com/methods/channels.info
+ */
+class GroupsInfoPayload extends AbstractPayload
+{
+    /**
+     * @var string
+     */
+    private $channel;
+
+    /**
+     * @param string $channelId
+     */
+    public function setChannelId($channelId)
+    {
+        $this->channel = $channelId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getChannelId()
+    {
+        return $this->channel;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMethod()
+    {
+        return 'groups.info';
+    }
+}
