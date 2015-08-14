906f084dcc9fa7039877e23f2e9ec5e0e6d9e394
Travis Raup <travis.raup@restek.com>
2015-08-14 09:09:27 -0400

    users.getPresence api method added to payload class.

---
+++ src/CL/Slack/Payload/UsersGetPresencePayload.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This user is part of the Slack API library.
+ *
+ * (c) Cas Leentfaar <info@casleentfaar.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * user that was distributed with this source code.
+ */
+
+namespace CL\Slack\Payload;
+
+/**
+ * @author Cas Leentfaar <info@casleentfaar.com>
+ *
+ * @link Official documentation at https://api.slack.com/methods/users.info
+ */
+class UsersGetPresencePayload extends AbstractPayload
+{
+    /**
+     * @var string $user ID of the user to get info on
+     */
+    private $user;
+
+    /**
+     * @param string $user ID of the user to get info on
+     */
+    public function setUserId($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return string ID of the user to get info on
+     */
+    public function getUserId()
+    {
+        return $this->user;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMethod()
+    {
+        return 'users.getPresence';
+    }
+}
