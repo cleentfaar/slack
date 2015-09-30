f3b82eef60d351f36d0d53fd054e3c78da4cc9ed
Travis Raup <travis.raup@restek.com>
2015-09-30 15:24:45 -0400

    Update ImOpenPayload to return channel id in the response

--- tests/src/CL/Slack/Tests/Payload/ImOpenPayloadResponseTest.php
+++ tests/src/CL/Slack/Tests/Payload/ImOpenPayloadResponseTest.php
@@ -27,6 +27,7 @@ class ImOpenPayloadResponseTest extends AbstractPayloadResponseTest
         return [
             'no_op' => true,
             'already_open' => true,
+            'channel' => array('id' => 'D12345678')
         ];
     }
 
@@ -40,5 +41,6 @@ class ImOpenPayloadResponseTest extends AbstractPayloadResponseTest
     {
         $this->assertEquals($responseData['no_op'], $payloadResponse->isNoOp());
         $this->assertEquals($responseData['already_open'], $payloadResponse->isAlreadyOpen());
+        $this->assertEquals($responseData['channel'], $payloadResponse->getChannel());
     }
 }
