3251822308e363bdf7a4940181b92c2702c6874d
Dries De Peuter <dries.depeuter@tobania.be>
2015-11-03 23:23:06 +0100

    Add undocumented users.admin.invite method.

---
+++ tests/src/CL/Slack/Tests/Payload/UsersAdminInvitePayloadResponseTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Slack API library.
+ *
+ * (c) Cas Leentfaar <info@casleentfaar.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CL\Slack\Tests\Payload;
+
+use CL\Slack\Payload\PayloadResponseInterface;
+use CL\Slack\Payload\UsersAdminInvitePayloadResponse;
+
+/**
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class UsersAdminInvitePayloadResponseTest extends AbstractPayloadResponseTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function createResponseData()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param array                           $responseData
+     * @param UsersAdminInvitePayloadResponse $payloadResponse
+     */
+    protected function assertResponse(array $responseData, PayloadResponseInterface $payloadResponse)
+    {
+    }
+}
---
+++ tests/src/CL/Slack/Tests/Payload/UsersAdminInvitePayloadTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Slack API library.
+ *
+ * (c) Cas Leentfaar <info@casleentfaar.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CL\Slack\Tests\Payload;
+
+use CL\Slack\Payload\PayloadInterface;
+use CL\Slack\Payload\UsersAdminInvitePayload;
+
+/**
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class UsersAdminInvitePayloadTest extends AbstractPayloadTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function createPayload()
+    {
+        $payload = new UsersAdminInvitePayload();
+        $payload->setEmail('test@domain.org');
+
+        return $payload;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param UsersAdminInvitePayload $payload
+     */
+    protected function getExpectedPayloadData(PayloadInterface $payload)
+    {
+        return [
+            'email' => $payload->getEmail(),
+        ];
+    }
+}
